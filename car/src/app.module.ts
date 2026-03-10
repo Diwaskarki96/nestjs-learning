680a7b49d16d769c1fa1e58385b4343459513484
Diwash Karki <diwaskarki96@gmail.com>
2026-03-10 23:23:34 +0545

    controllers, modules and serices

--- car/src/app.module.ts
+++ car/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
