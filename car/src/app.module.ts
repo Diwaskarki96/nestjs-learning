548f1adc3596df0e099c56f329211bcd8e108b76
Diwash Karki <diwaskarki96@gmail.com>
2026-03-11 22:07:40 +0545

    Database setup

--- car/src/app.module.ts
+++ car/src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportModule } from './report/report.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ReportModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
